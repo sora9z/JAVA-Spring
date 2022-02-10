5683c0814afecbfb26d9ee410b9c46b61a20a50e
sora9z <sssssssorasss@gmail.com>
2022-02-11 02:00:06 +0900

    Spring Boot ch04 spring boot start

---
+++ practice/src/main/java/com/example/hello/dto/UserRequest.java
@@ -0,0 +1,41 @@
+package com.example.hello.dto;
+
+public class UserRequest {
+
+    private static String name;
+    private static String email;
+    private static int age;
+
+    public static String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public static int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRequest{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
